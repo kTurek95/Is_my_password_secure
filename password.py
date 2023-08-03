45800373485fd54e86a42d752c8be1957b5495a7
kTurek95 <k.turek1995@gmail.com>
2023-08-03 23:10:22 +0100

    Adding docstrings to the functions in the Password class

--- password.py
+++ password.py
@@ -1,18 +1,50 @@
+""" Module with Password class """
+
+
 class ValidationException(Exception):
-    pass
+    """
+    Custom exception class to represent validation errors in the Password class.
+    It is raised when the password fails one or more validation checks.
+    """
 
 
 class Password:
+    """
+    The Password class represents a user password and provides various methods
+    to validate its strength. It checks for password length, presence of numbers,
+     special characters, uppercase and lowercase characters.
+    """
     def __init__(self, user_password):
+        """
+        Initializes a new Password object with the given user password.
+
+        :param user_password: The user's password to be validated.
+        :type user_password: str
+        """
         self.user_password = user_password
 
     def correct_length(self):
+        """
+        Checks if the password has a minimum length of 8 characters.
+
+        :return: True if the password has a minimum length of 8 characters.
+        :rtype: bool
+        :raises ValidationException: If the password is too short.
+        """
+
         if len(self.user_password) >= 8:
             return True
 
         raise ValidationException('Password is to short')
 
     def at_least_one_number(self):
+        """
+        Checks if the password contains at least one digit.
+
+        :return: True if the password contains at least one digit.
+        :rtype: bool
+        :raises ValidationException: If at least one digit is missing.
+        """
         numbers = '0123456789'
         for letter in self.user_password:
             if letter in numbers:
@@ -21,6 +53,13 @@ class Password:
         raise ValidationException('Missing at least one number')
 
     def at_least_one_special_character(self):
+        """
+        Checks if the password contains at least one special character.
+
+        :return: True if the password contains at least one special character.
+        :rtype: bool
+        :raises ValidationException: If at least one special character is missing.
+        """
         spec_char = '!@#$%^&*<>?|()'
         for letter in self.user_password:
             if letter in spec_char:
@@ -29,6 +68,13 @@ class Password:
         raise ValidationException('Missing at least one special character')
 
     def upper_and_lower_characters(self):
+        """
+        Checks if the password contains at least one uppercase letter and one lowercase letter.
+
+        :return: True if the password contains at least one uppercase and one lowercase letter.
+        :rtype: bool
+        :raises ValidationException: If at least one uppercase or one lowercase letter is missing.
+        """
         is_lower = any(letter.islower() for letter in self.user_password)
         is_upper = any(letter.isupper() for letter in self.user_password)
 
