d4d8642c03f007f95e4d2f53ea69556f7783e73c
kTurek95 <k.turek1995@gmail.com>
2023-08-10 20:33:20 +0100

    Adding docstring

--- password_tests.py
+++ password_tests.py
@@ -1,16 +1,23 @@
-''' Module with tests for Password class '''
-
+""" Module with tests for Password class """
 import pytest
 from password import Password, ValidationException
 
 
 def test_correct_length_positive():
+    """
+    Tests the correct_length() method of the Password class with a valid password length.
+    Checks if the method returns True when the password has the correct length.
+    """
     correct_password = Password('Kacper123')
 
     assert Password.correct_length(correct_password) is True
 
 
 def test_correct_length_negative():
+    """
+   Tests the correct_length() method of the Password class with an invalid password length.
+   Checks if the method raises a ValidationException with an appropriate message when the password is too short.
+   """
     wrong_password = Password('Kacper')
 
     with pytest.raises(ValidationException) as error:
@@ -20,12 +27,20 @@ def test_correct_length_negative():
 
 
 def test_at_least_one_number_positive():
+    """
+    Tests the at_least_one_number() method of the Password class with a valid password containing at least one number.
+    Checks if the method returns True when the password contains at least one number.
+    """
     correct_password = Password('Kacp3r')
 
     assert Password.at_least_one_number(correct_password) is True
 
 
 def test_at_least_one_number_negative():
+    """
+    Tests the at_least_one_number() method of the Password class with an invalid password missing numbers.
+    Checks if the method raises a ValidationException with an appropriate message when the password does not contain any numbers.
+    """
     wrong_password = Password('Kacper')
 
     with pytest.raises(ValidationException) as error:
@@ -35,12 +50,20 @@ def test_at_least_one_number_negative():
 
 
 def test_at_least_one_special_character_positive():
+    """
+   Tests the at_least_one_special_character() method of the Password class with a valid password containing at least one special character.
+   Checks if the method returns True when the password contains at least one special character.
+   """
     correct_password = Password('Kacp#r')
 
     assert Password.at_least_one_special_character(correct_password) is True
 
 
 def test_at_least_one_special_character_negative():
+    """
+   Tests the at_least_one_special_character() method of the Password class with an invalid password missing special characters.
+   Checks if the method raises a ValidationException with an appropriate message when the password does not contain any special characters.
+   """
     wrong_password = Password('Kacper')
 
     with pytest.raises(ValidationException):
@@ -50,12 +73,20 @@ def test_at_least_one_special_character_negative():
 
 
 def test_upper_and_lower_characters_positive():
+    """
+    Tests the upper_and_lower_characters() method of the Password class with a valid password containing both upper and lower characters.
+    Checks if the method returns True when the password contains both upper and lower characters.
+    """
     correct_password = Password('Kacper')
 
     assert Password.upper_and_lower_characters(correct_password) is True
 
 
 def test_upper_and_lower_characters_negative_lower():
+    """
+    Tests the upper_and_lower_characters() method of the Password class with an invalid password missing lowercase characters.
+    Checks if the method raises a ValidationException with an appropriate message when the password does not contain any lowercase characters.
+    """
     upper_password = Password('KACPER')
 
     with pytest.raises(ValidationException) as error:
@@ -65,6 +96,10 @@ def test_upper_and_lower_characters_negative_lower():
 
 
 def test_upper_and_lower_characters_negative_upper():
+    """
+   Tests the upper_and_lower_characters() method of the Password class with an invalid password missing uppercase characters.
+   Checks if the method raises a ValidationException with an appropriate message when the password does not contain any uppercase characters.
+   """
     lower_password = Password('kacper')
 
     with pytest.raises(ValidationException) as error:
