73406ae65683e2b0d348f80bd0218fd5c88eb177
kTurek95 <k.turek1995@gmail.com>
2023-08-12 13:44:09 +0100

    Installation package containing secure password requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='password',
+    version='0.1',
+    description='Collection of password and api class',
+    author='Kacper Turek',
+    packages=['password_package'],
+    install_requires=['requests']
+)
